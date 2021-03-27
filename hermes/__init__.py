ecec68084f5ab2bf92afc79cdc72c5cafd525ea2
Anoop Felix <anooptiger@hotmail.com>
2021-03-27 19:46:49 +0800

    basic

---
+++ hermes/__init__.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, render_template
+
+
+def create_app(test_config=None):
+    # create app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "hermes.sqlite")
+    )
+
+    # ensure the instance folder exist
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/')
+    def index():
+        return render_template("index.html")
+
+    from . import db
+    db.init_app(app)
+
+    return app
